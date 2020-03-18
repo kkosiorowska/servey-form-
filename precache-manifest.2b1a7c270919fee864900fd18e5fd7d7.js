2a591c136836c03cc631fab8e38d86e020f170e2
kkosiorowska <kaarolina.kosiorowska@gmail.com>
2020-03-18 14:40:08 +0100

    Updates

---
+++ precache-manifest.2b1a7c270919fee864900fd18e5fd7d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a26071a94c6be914a9aa6e00a0e294b",
+    "url": "/servey-form/index.html"
+  },
+  {
+    "revision": "08bcf4899d5cc2a2c178",
+    "url": "/servey-form/static/css/main.e26a2d73.chunk.css"
+  },
+  {
+    "revision": "5174d459db6b17207540",
+    "url": "/servey-form/static/js/2.4de74d65.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/servey-form/static/js/2.4de74d65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08bcf4899d5cc2a2c178",
+    "url": "/servey-form/static/js/main.45a3e63a.chunk.js"
+  },
+  {
+    "revision": "418bc30539b69ac5f7e8",
+    "url": "/servey-form/static/js/runtime-main.38c2c7f8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/survey-form/precache-manifest.c458a62c89021106277b620670866aac.js"
+  "/servey-form/precache-manifest.2b1a7c270919fee864900fd18e5fd7d7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/survey-form/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/servey-form/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
